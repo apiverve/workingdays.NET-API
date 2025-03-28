cb4880494c8d36dd024fdc30a6b26d9321a7cf4a
APIVerve Team <hello@apiverve.com>
2025-03-28 10:48:22 -0500

    Bug Fixes

---
+++ WorkingDaysQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class WorkingDaysQueryOptions {
+public string country { get; set; }
+public integer year { get; set; }
+public integer month { get; set; }
+}
+
+}
